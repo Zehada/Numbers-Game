bef6dac2c8911a4825c543076a77b1dfd53c8ac5
Oguzhan Oncu <oguzhan.oncu7@gmail.com>
2025-03-18 15:48:17 +0100

    Add the feature to randomly generate numbers as in the show

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Inputs from "./components/Inputs";
+import NumberToPlayWith from "./components/NumberToPlayWith";
 
 function App() {
-  const numbers = [6, 45, 2, 6, 63, 10];
-  const [numbersList, setNumbersList] = useState<
-    { id: number; number: number }[]
-  >(
-    [...numbers]
-      .sort(function (a, b) {
-        return a - b;
-      })
-      .map((number, index) => ({ id: index + 1, number: number }))
-  );
+  // const numbers = [6, 45, 2, 6, 63, 10];
+  const [numbers, setNumbers] = useState<number[]>([]);
+
+  const [numbersList, setNumbersList] = useState([{ id: 0, number: 0 }]);
+
+  // const [numbersList, setNumbersList] = useState<
+  //   { id: number; number: number }[]
+  // >(
+  //   [...numbers]
+  //     .sort(function (a, b) {
+  //       return a - b;
+  //     })
+  //     .map((number, index) => ({ id: index + 1, number: number }))
+  // );
+
+  useEffect(() => {
+    setNumbersList(
+      [...numbers]
+        .sort(function (a, b) {
+          return a - b;
+        })
+        .map((number, index) => ({ id: index + 1, number: number }))
+    );
+  }, [numbers]);
+
+  function handleNumbers(data: number[]) {
+    setNumbers(data);
+  }
 
   function handleNumbersLeft(data: { id: number; number: number }[]) {
     setNumbersList(data);
   }
 
+  console.log(numbers);
+
   return (
     <>
       <Inputs numbersList={numbersList} numbersLeft={handleNumbersLeft} />
+      <NumberToPlayWith randomNumbers={handleNumbers} />
     </>
   );
 }
---
+++ src/components/NumberToPlayWith.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+type NumbersToPlayWithProps = {
+  randomNumbers: any;
+};
+
+function NumbersToPlayWith({ randomNumbers }: NumbersToPlayWithProps) {
+  let listOfNumber = [
+    1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75,
+    100,
+  ];
+
+  let selectedNumbers: number[] = [];
+  let randomIndex = 0;
+
+  for (let index = 0; index < 6; index++) {
+    randomIndex = Math.floor(Math.random() * listOfNumber.length);
+    selectedNumbers.push(listOfNumber[randomIndex]);
+    listOfNumber.splice(randomIndex, 1);
+  }
+
+  useEffect(() => {
+    randomNumbers(selectedNumbers);
+  }, []);
+
+  return <div>hi</div>;
+}
+
+export default NumbersToPlayWith;
